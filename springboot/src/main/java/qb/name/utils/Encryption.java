691f7af4e15a724ee56459829565f01d6a975843
悬崖的鹰 <qtsyouxiang@163.com>
2017-01-24 11:42:24 +0800

    封装guava的md5加密方法

---
+++ springboot/src/main/java/qb/name/utils/Encryption.java
@@ -0,0 +1,21 @@
+﻿package qb.name.utils;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
+import com.google.common.hash.Hashing;
+
+/**
+ * @author qb
+ * @description:
+ * @date 2017-1-24 11:23
+ */
+public class Encryption {
+
+    // 封装guava的md5加密方法
+    public static String mixMd5(String password){
+        if(Strings.isNullOrEmpty(password)){
+            return "";
+        }
+        return Hashing.md5().newHasher().putString(password, Charsets.UTF_8).hash().toString();
+    }
+}
